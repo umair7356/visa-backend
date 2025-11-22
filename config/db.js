ce309b862bd62fb84afce76533e5910753267f3c
umair7356 <149939960+umair7356@users.noreply.github.com>
2025-11-22 21:31:12 +0500

    ..

--- config/db.js
+++ config/db.js
@@ -9,7 +9,10 @@ const connectDB = async () => {
   }
 
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     isConnected = conn.connections[0].readyState;
     console.log("MongoDB Connected");
   } catch (error) {
